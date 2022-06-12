16ac75aa66d5d8fa668e71b237ebda2f3911d3c4
DanielMalede <danielmalede@gmail.com>
2022-06-12 21:38:48 +0300

    .

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-// chackUserName()==chackUserDate()
-// function chackAllFun() {
-//     if (chackUserPassword()==true) {
-//         return true
-//     }
-//     return false
-// }
+chackUserName()==chackUserDate()
+function chackAllFun() {
+    if (chackUserName()==true) {
+        return true
+    }
+    return false
+}
 
-// var userName =document.getElementById("userName")
-// var userLastName =document.getElementById("lastName")
-// function chackUserName() {
-//     var limit10D = /^[ A-Za-z]{10}$/
-//     if (limit10D.test(userName.value)&&limit10D.test(lastName.value)) {
-//         userName.style.borderColor="green"
-//         lastName.style.borderColor="green"
-//         return true
-//     }
-//     userName.style.borderColor="red"
-//     lastName.style.borderColor="red"    
-//     return false
-// }
+var userName =document.getElementById("userName")
+var userLastName =document.getElementById("lastName")
+function chackUserName() {
+    var limit10D = /^[ A-Za-z]{10}$/
+    if (limit10D.test(userName.value)&&limit10D.test(lastName.value)) {
+        userName.style.borderColor="green"
+        lastName.style.borderColor="green"
+        return true
+    }
+    userName.style.borderColor="red"
+    lastName.style.borderColor="red"    
+    return false
+}
 
 // var userDate =document.getElementById("userDate")
 // function chackUserDate() {
@@ -41,16 +41,16 @@
 //     return false
 // }
 
-// var userPhone =document.getElementById("userPhone")
-// function chackUserPhone() {
-//     var phoneRegex = /^\d{10}$/;    
-//     if (phoneRegex.test(userPhone.value)) {
-//     alert("yes")
-//     return true
-//     }
-//     alert("no")
-//     return false
-// }
+var userPhone =document.getElementById("userPhone")
+function chackUserPhone() {
+    var phoneRegex = /^\d{10}$/;    
+    if (phoneRegex.test(userPhone.value)) {
+    alert("yes")
+    return true
+    }
+    alert("no")
+    return false
+}
 
 // var userPassword1 =document.getElementById("userPassword1")
 // function chackUserPassword() {
