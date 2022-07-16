494940270c060bdb2f6794cecdd65085ed82f543
DanielMalede <danielmalede@gmail.com>
2022-07-16 21:50:12 +0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-chackUserName()==chackUserDate()
 function chackAllFun() {
-    if (chackUserName()==true) {
+    if (chackUserDate()==true) {
         return true
     }
     return false
@@ -20,26 +19,26 @@ function chackUserName() {
     return false
 }
 
-// var userDate =document.getElementById("userDate")
-// function chackUserDate() {
-//     var newDate = new Date().getFullYear()
-//     if (newDate-userDate.value.substr(0,4)>18) {
-//         return true
-//     }
-    
-//     return false
-// }
+var userDate =document.getElementById("userDate")
+function chackUserDate() {
+    var newDate = new Date().getFullYear()
+    if (newDate-userDate.value.substr(0,4)>18) {
+        return true
+    }
+    alert("no")
+    return false
+}
 
-// var userEmail =document.getElementById("userEmail")
-// function chackUserEmail() {
-//     var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-//     if (emailRegex.test(userEmail.value)) {
-//     userEmail.style.borderColor="green"
-//         return true
-//     }
-//     userEmail.style.borderColor="red"
-//     return false
-// }
+var userEmail =document.getElementById("userEmail")
+function chackUserEmail() {
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if (emailRegex.test(userEmail.value)) {
+    userEmail.style.borderColor="green"
+        return true
+    }
+    userEmail.style.borderColor="red"
+    return false
+}
 
 var userPhone =document.getElementById("userPhone")
 function chackUserPhone() {
@@ -52,12 +51,12 @@ function chackUserPhone() {
     return false
 }
 
-// var userPassword1 =document.getElementById("userPassword1")
-// function chackUserPassword() {
-//     if (userPassword1.value[0].UpperCase()==true) {
-//         alert("yes")
-//         return true
-//     }
-//     alert("no")
-//     return false
-// }
+var userPassword1 =document.getElementById("userPassword1")
+function chackUserPassword() {
+    if (userPassword1.value[0].UpperCase()==true) {
+        alert("yes")
+        return true
+    }
+    alert("no")
+    return false
+}
